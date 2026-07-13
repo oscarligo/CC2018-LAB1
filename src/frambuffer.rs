3532721e8f70ba40506086bef51f9c3bfc7cb744
Oscar Rompich <oscarligo6@gmail.com>
2026-07-13 16:55:23 -0600

    feat: framebuffer implementation

---
+++ src/frambuffer.rs
@@ -0,0 +1,44 @@
+use raylib::prelude::*;
+
+pub struct Framebuffer {
+    pub width: u32,
+    pub height: u32,
+    pub color_buffer: Image,
+    background_color: Color,
+    current_color: Color,
+}
+
+impl Framebuffer {
+    pub fn new(width: u32, height: u32, background_color: Color) -> Self {
+        let color_buffer = Image::gen_image_color(width, height, background_color);
+        Framebuffer {
+            width,
+            height,
+            color_buffer,
+            background_color,
+            current_color: Color::WHITE,
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.color_buffer = Image::gen_image_color(self.width, self.height, self.background_color);
+    }   
+
+    pub fn set_pixel(&mut self, x: u32, y: u32, color: Color) {
+        if x < self.width && y < self.height {
+            self.color_buffer.set_pixel(x, y, color);
+        }
+    }
+
+    pub fn set_current_color(&mut self, color: Color) {
+        self.current_color = color;
+    }
+
+    pub fn set_background_color(&mut self, color: Color) {
+        self.background_color = color;
+    }
+
+    pub fn render_to_file(&self, filename: &str) {
+        self.color_buffer.export_image(filename);
+    }   
+}
