25553afdae032f1e58aceb00b2da6bdaa3787bdc
Oscar Rompich <oscarligo6@gmail.com>
2026-07-14 18:31:26 -0600

    feat: draw polygon implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 mod framebuffer;
 mod line;
+mod polygon;
 
 use raylib::prelude::*;
 use framebuffer::Framebuffer;
 use line::line;
+use polygon::polygon;
 
 fn main() {
     let mut framebuffer = Framebuffer::new(800, 600, Color::BLACK);
     framebuffer.clear();
-    line(&mut framebuffer, Vector2::new(100.0, 100.0), Vector2::new(200.0, 200.0));
+    line(&mut framebuffer, Vector2::new(100.0, 100.0), Vector2::new(200.0, 200.0));   
+
+    let vertices: Vec<Vector2> = vec![
+        Vector2::new(165.0, 380.0),
+        Vector2::new(185.0, 360.0),
+        Vector2::new(180.0, 330.0),
+        Vector2::new(207.0, 345.0),
+        Vector2::new(233.0, 330.0),
+        Vector2::new(230.0, 360.0),
+        Vector2::new(250.0, 380.0),
+        Vector2::new(220.0, 385.0),
+        Vector2::new(205.0, 410.0),
+        Vector2::new(193.0, 383.0),
+    ];
+
+    
+    // Draw a polygon
+    polygon(&mut framebuffer, &vertices);   
+
     framebuffer.render_to_file("output.png");
+
 }
--- src/polygon.rs
+++ src/polygon.rs
@@ -1,3 +1,18 @@
+use raylib::prelude::*;
 use crate::line::line;
+use crate::framebuffer::Framebuffer;
+
+pub fn polygon(
+    framebuffer: &mut Framebuffer,
+    vertices: &[Vector2],
+) {
+    let num_vertices = vertices.len();
+    for i in 0..num_vertices {
+        let start = vertices[i];
+        let end = vertices[(i + 1) % num_vertices]; 
+        line(framebuffer, start, end);
+    }
+}   
+
 
 
